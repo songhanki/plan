9d86a278cda8db8ec532db09bb14a55bcb2aa92d
HanKi Song <hksong9276@gmail.com>
2025-07-27 20:44:04 +0900

    mybatis-config 설정 및  dto 축약어 설정 진행행

--- src/main/java/com/board/plan/PlanApplication.java
+++ src/main/java/com/board/plan/PlanApplication.java
@@ -2,8 +2,10 @@ package com.board.plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(scanBasePackages = {"com.board.plan.core", "com.board.plan.user"})
+@SpringBootApplication
+@MapperScan(basePackages = "com.board.plan.*.mapper")
 public class PlanApplication {
 
 	public static void main(String[] args) {
