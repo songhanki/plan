15e1d8904c83362b461622da6e59469b19adbd6a
HanKi Song <hksong9276@gmail.com>
2025-08-11 22:49:37 +0900

    회원조회 쿼리 수정정

--- src/main/java/com/board/plan/auth/controller/TokenController.java
+++ src/main/java/com/board/plan/auth/controller/TokenController.java
@@ -11,11 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @Slf4j
 @RestController
